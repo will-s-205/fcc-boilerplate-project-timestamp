bfb01cbc6c2c1a798c1bc9bef527e008c204a058
William S <will.step.205@gmail.com>
2023-05-19 20:02:12 -0700

    setup for local runs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+require('dotenv').config(); // for local env var
 var express = require('express');
 var app = express();
 
@@ -9,7 +10,6 @@ var app = express();
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 
-require('dotenv').config(); // for local env var
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -42,6 +42,9 @@ app.get("/api/hello", function (req, res) {
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
+  const PORT = 8080; // for local runs
+  // const po = listener.address().port;
   // console.log('Your app is listening on port ' + listener.address().port);
-  console.log(`server live now on http://localhost:${process.env.PORT}`) // for local env var
+  // console.log(`server live now on http://localhost:${process.env.PORT}`) // for local env var PORT=8080
+  console.log(`DEBUGGING ON LOCAL: http://localhost:${listener.address().port}`) // for local runs
 });
