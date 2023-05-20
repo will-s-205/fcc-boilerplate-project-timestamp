83b336301873f997cd5c0bffdca5f2d0d82fa6a0
William S <will.step.205@gmail.com>
2023-05-19 19:13:28 -0700

    JASON date output

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+
+require('dotenv').config(); // for local env var
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,15 +20,28 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", (req, res) => {
+  const date = new Date();
+
+  // debug
+  unix = date.toUTCString();
+  utc = date.getTime();
+  console.log({date, utc, unix});
+
+  // An empty date parameter returns the current time in a JSON object with a unix key and utc
+  return res.json ({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  // console.log('Your app is listening on port ' + listener.address().port);
+  console.log(`server live now on http://localhost:${process.env.PORT}`) // for local env var
 });
